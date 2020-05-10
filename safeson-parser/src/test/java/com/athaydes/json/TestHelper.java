8dc9235ca9c1f455e5eacb7316a4fc0092f6520e
renatoathaydes <renatoathaydes@hotmail.com>
2020-05-10 11:13:55 +0200

    Better test failure message when Exception thrown is not as expected.

--- safeson-parser/src/test/java/com/athaydes/json/TestHelper.java
+++ safeson-parser/src/test/java/com/athaydes/json/TestHelper.java
@@ -3,12 +3,18 @@ package com.athaydes.json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.function.Executable;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public interface TestHelper {
     default void assertThrowsJsonException(Executable executable,
                                            String expectedMessage,
                                            int index) {
         var ex = Assertions.assertThrows(JsonException.class, executable);
         var msg = new JsonException(index, expectedMessage).getMessage();
-        Assertions.assertEquals(msg, ex.getMessage());
+        if (!msg.equals(ex.getMessage())) {
+            fail("Expected : " + msg + "\nActual   : " + ex.getMessage(), ex);
+        }
+        assertEquals(msg, ex.getMessage());
     }
 }
